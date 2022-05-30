63735f77ca681e2f06e3e7dc11f9c29d988494e9
BohdanChernyshev <99904629+BohdanChernyshev@users.noreply.github.com>
2022-05-30 18:14:01 +0300

    Add files via upload

---
+++ ControllerExtensibility/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿//using System.Web.Mvc;
+//using ControllerExtensibility.Models;
+
+//namespace ControllerExtensibility.Controllers
+//{
+//    public class CustomerController : Controller
+//    {
+//        public ViewResult Index()
+//        {
+//            return View("Result", new Result
+//            {
+//                ControllerName = "Customer",
+//                ActionName = "Index"
+//            });
+//        }
+
+//        public ViewResult List()
+//        {
+//            return View("Result", new Result
+//            {
+//                ControllerName = "Customer",
+//                ActionName = "List"
+//            });
+//        }
+//    }
+//}
+
+using System.Web.Mvc;
+using ControllerExtensibility.Models;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // ...
+
+        [ActionName("Enumerate")]
+        public ViewResult List()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "List"
+            });
+        }
+    }
+}
